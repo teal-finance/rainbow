c1fa32b2ed78be91925b35c40a0c52099a6662c4
Oliver <0uep@hessling.fr>
2021-10-07 00:38:37 +0200

    Add a licence header on top of the source files

--- cmd/serum/serum.go
+++ cmd/serum/serum.go
@@ -1,18 +1,8 @@
-// copied from https://github.com/streamingfast/solana-go/blob/master/cmd/slnc/cmd/serum_get_market.go
-
-// Copyright 2020 dfuse Platform Inc.
+// Copyright (c) 2021 teal.finance
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 package main
 
