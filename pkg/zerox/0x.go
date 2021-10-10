499924db2a57ef1e5db3efdd6728680928fabacf
Oliver <0uep@hessling.fr>
2021-10-10 04:34:47 +0200

    Deduce the server rate limits

--- pkg/zerox/0x.go
+++ pkg/zerox/0x.go
@@ -14,6 +14,7 @@ import (
 	"math"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/teal-finance/rainbow"
@@ -44,57 +45,193 @@ var decimalsToPower10 = map[int]float64{
 	12: 100000000000,
 }
 
-func extract(i getOptionsOtokensOToken) (tipe, expiry string, strike float64) {
-	tipe = "CALL"
+func extract(i getOptionsOtokensOToken) (oType, expiry string, strike float64) {
+	oType = "CALL"
 	if i.IsPut {
-		tipe = "PUT"
+		oType = "PUT"
 	}
 
 	seconds, err := strconv.ParseInt(i.ExpiryTimestamp, 10, 0)
-	expiryTime := time.Unix(seconds, 0).UTC()
-	expiry = expiryTime.Format("2006-01-02 15:04:05")
-	if err != nil {
+	if err == nil {
+		expiryTime := time.Unix(seconds, 0).UTC()
+		expiry = expiryTime.Format("2006-01-02 15:04:05")
+	} else {
 		log.Printf("WARN Expiry: %v from %+v", err, i)
-		expiry = ""
 	}
 
 	strikeInt, err := strconv.ParseInt(i.StrikePrice, 10, 0)
-	if err != nil {
-		log.Printf("WARN Strike: %v from %+v", err, i)
-	} else {
+	if err == nil {
 		denominateur, ok := decimalsToPower10[i.StrikeAsset.Decimals]
 		if ok {
 			strike = float64(strikeInt) / denominateur
 		}
+	} else {
+		log.Printf("WARN Strike: %v from %+v", err, i)
 	}
 
 	return
 }
 
-func GetOrderBook(instruments []getOptionsOtokensOToken, provider string) ([]rainbow.Options, error) {
-	baseURL := "https://api.0x.org/orderbook/v1?quoteToken="
-	opts := "&baseToken=" + USDC
+const (
+	baseURL = "https://api.0x.org/orderbook/v1?quoteToken="
+	opts    = "&baseToken=" + USDC
+)
 
+func getOrderbook(i getOptionsOtokensOToken) (result OrderBook, err error) {
+	var resp *http.Response
+	if resp, err = http.Get(baseURL + i.Id + opts); err != nil {
+		err = fmt.Errorf("base=%v id=%v opts=%v: %w", baseURL, i.Id, opts, err)
+		return
+	}
+
+	var j []byte
+	j, err = io.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+		err = fmt.Errorf("cannot ReadAll: %w", err)
+		return
+	}
+
+	if err = json.Unmarshal(j, &result); err != nil {
+		err = fmt.Errorf("WARN %w when json.Unmarshal: %v", err, string(j))
+	}
+
+	return
+}
+
+// getQuote you can get the quote in the side you want with 0x
+// but for us we will focus on always buying or selling a certain amount of options
+// so Asks/SELL side (so you send a buy inquiry): sellToken=usdc, buyToken=option address
+// so Bids/BUY side (so you send a sell inquiry): sellToken=option address, buyToken=usdc.
+func getQuote(side, sellToken, buyToken string, amount float64, decimals int) (Quote, error) {
+	baseURL := "https://api.0x.org/swap/v1/quote?sellToken=" + sellToken + "&buyToken=" + buyToken
+	if side == "SELL" {
+		baseURL += "&buyAmount="
+	} else {
+		baseURL += "&sellAmount="
+	}
+	baseURL += ConvertToSolidity(amount, decimals)
+
+	resp, err := http.Get(baseURL)
+	if err != nil {
+		return Quote{}, err
+	}
+
+	j, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+		return Quote{}, fmt.Errorf("cannot ReadAll quote from Ox: %w", err)
+	}
+
+	var result Quote
+	if err = json.Unmarshal(j, &result); err != nil {
+		return Quote{}, fmt.Errorf("cannot json.Unmarshal quote from Ox: %w in: %v", err, string(j))
+	}
+
+	return result, nil
+}
+
+type stubbornRequester struct {
+	sleep  time.Duration
+	maxBad time.Duration
+	ok     int
+	ko     int
+}
+
+var defaultStubbornRequester = stubbornRequester{
+	sleep:  300 * time.Millisecond,
+	maxBad: 200 * time.Millisecond,
+	ok:     0,
+	ko:     0,
+}
+
+func (sr *stubbornRequester) success(n int) {
+	sr.ok++
+
+	if sr.ok > 1 {
+		var newSleep time.Duration
+		if sr.sleep > sr.maxBad {
+			newSleep = sr.sleep - (sr.sleep-sr.maxBad)/32
+		} else {
+			newSleep = sr.maxBad
+		}
+
+		if n > 0 {
+			log.Printf("Success #%v n=%v bad=%v sleep=%v (%+v)",
+				sr.ok, n, sr.maxBad, newSleep, newSleep-sr.sleep)
+		}
+
+		sr.sleep = newSleep
+		sr.maxBad -= sr.maxBad / 128
+	}
+}
+
+func (sr *stubbornRequester) failure(err error) {
+	newSleep := sr.maxBad / 2
+	sr.ko++
+
+	log.Printf("Failure #%v bad=%v sleep=%v + %v %v",
+		sr.ko, sr.maxBad, sr.sleep, newSleep,
+		strings.ReplaceAll(strings.ReplaceAll(err.Error(), "\n", ""), "\r", ""))
+
+	sr.maxBad = sr.sleep + newSleep
+	sr.sleep = newSleep
+}
+
+func (sr *stubbornRequester) getOrderBook(i getOptionsOtokensOToken) (ob OrderBook, err error) {
+	for n := 0; n < 22; n++ {
+		if sr.ok+sr.ko > 0 {
+			time.Sleep(sr.sleep)
+		}
+
+		ob, err = getOrderbook(i)
+
+		if err == nil {
+			sr.success(n)
+			break
+		}
+
+		sr.failure(err)
+	}
+
+	return
+}
+
+func (sr *stubbornRequester) getQuote(side, sellToken, buyToken string, amount float64, decimals int) (q Quote, err error) {
+	for n := 0; n < 22; n++ {
+		if sr.ok+sr.ko > 0 {
+			time.Sleep(sr.sleep)
+		}
+
+		q, err = getQuote(side, sellToken, buyToken, amount, decimals)
+
+		if err == nil {
+			sr.success(n)
+			break
+		}
+
+		sr.failure(err)
+	}
+
+	return
+}
+
+func (sr *stubbornRequester) logStats() {
+	log.Printf("stats: bad=%v sleep=%v ok=%v ko=%v",
+		sr.maxBad, sr.sleep, sr.ok, sr.ko)
+}
+
+func GetOrderBook(instruments []getOptionsOtokensOToken, provider string) ([]rainbow.Options, error) {
 	orderBook := []rainbow.Options{}
 
+	sr := defaultStubbornRequester
+
 	for _, i := range instruments {
-		resp, err := http.Get(baseURL + i.Id + opts)
+		ob, err := sr.getOrderBook(i)
 		if err != nil {
-			log.Printf("WARN Get base=%v id=%v opts=%v: %v", baseURL, i.Id, opts, err)
-			continue
-		}
-
-		j, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-
-		if err != nil {
-			log.Print("WARN cannot ReadAll: ", err)
-			continue
-		}
-
-		var result OrderBook
-		if err = json.Unmarshal(j, &result); err != nil {
-			log.Printf("WARN %v when json.Unmarshal: %v", err, string(j))
+			log.Print("getOrderBook ", err)
 			continue
 		}
 
@@ -113,22 +250,24 @@ func GetOrderBook(instruments []getOptionsOtokensOToken, provider string) ([]rai
 			Offers:       nil,
 		}
 
-		b, err := BidsAsksToOffers(result.Bids.Records, "BUY", i.UnderlyingAsset.Symbol)
+		b, err := BidsAsksToOffers(ob.Bids.Records, "BUY", i.UnderlyingAsset.Symbol)
 		if err != nil {
 			return []rainbow.Options{}, err
 		}
 
-		a, err := BidsAsksToOffers(result.Asks.Records, "SELL", i.UnderlyingAsset.Symbol)
+		a, err := BidsAsksToOffers(ob.Asks.Records, "SELL", i.UnderlyingAsset.Symbol)
 		if err != nil {
 			return []rainbow.Options{}, err
 		}
 
 		o.Offers = append(o.Offers, b...)
 		o.Offers = append(o.Offers, a...)
-		// fmt.Println("o ", o)
+		// log.Println("o ", o)
 
 		orderBook = append(orderBook, o)
 	}
+
+	sr.logStats()
 
 	return orderBook, nil
 }
@@ -207,43 +346,10 @@ func BidsAsksToOffers(records Records, side, quote string) ([]rainbow.Offer, err
 	return offers, nil
 }
 
-// GetQuote you can get the quote in the side you want with 0x
-// but for us we will focus on always buying or selling a certain amount of options
-// so Asks/SELL side (so you send a buy inquiry): sellToken=usdc, buyToken=option address
-// so Bids/BUY side (so you send a sell inquiry): sellToken=option address, buyToken=usdc.
-func GetQuote(side, sellToken, buyToken string, amount float64, decimals int) (Quote, error) {
-	baseURL := "https://api.0x.org/swap/v1/quote?sellToken=" + sellToken + "&buyToken=" + buyToken
-	if side == "SELL" {
-		baseURL += "&buyAmount="
-	} else {
-		baseURL += "&sellAmount="
-	}
-	baseURL += ConvertToSolidity(amount, decimals)
-
-	fmt.Println("swap url ", baseURL)
-
-	resp, err := http.Get(baseURL)
-	if err != nil {
-		return Quote{}, err
-	}
-
-	j, err := io.ReadAll(resp.Body)
-	resp.Body.Close()
-
-	if err != nil {
-		return Quote{}, fmt.Errorf("cannot ReadAll quote from Ox: %w", err)
-	}
-
-	var result Quote
-	if err = json.Unmarshal(j, &result); err != nil {
-		return Quote{}, fmt.Errorf("cannot json.Unmarshal quote from Ox: %w in: %v", err, string(j))
-	}
-
-	return result, nil
-}
-
 func GetAggregatedOrderBook(instruments []getOptionsOtokensOToken, provider string, amount float64) ([]rainbow.Options, error) {
 	orderBook := []rainbow.Options{}
+
+	sr := defaultStubbornRequester
 
 	for _, i := range instruments {
 		tipe, expiry, strike := extract(i)
@@ -268,16 +374,17 @@ func GetAggregatedOrderBook(instruments []getOptionsOtokensOToken, provider stri
 			Offers:       nil,
 		}
 
-		b, err := GetQuote("BUY", i.Id, USDC, amount, decimals)
+		b, err := sr.getQuote("BUY", i.Id, USDC, amount, decimals)
 		if err != nil {
+			log.Print("getQuote BUY ", err)
 			return []rainbow.Options{}, err
 		}
 
 		if b.Price != "" {
 			price, err := strconv.ParseFloat(b.Price, 64)
 			if err != nil {
-				fmt.Println("price ")
-				return []rainbow.Options{}, err
+				log.Print("WARN price ", err)
+				continue
 			}
 
 			o.Offers = append(o.Offers, rainbow.Offer{
@@ -295,8 +402,9 @@ func GetAggregatedOrderBook(instruments []getOptionsOtokensOToken, provider stri
 			})
 		}
 
-		a, err := GetQuote("SELL", USDC, i.Id, amount, decimals)
+		a, err := sr.getQuote("SELL", USDC, i.Id, amount, decimals)
 		if err != nil {
+			log.Print("getQuote SELL ", err)
 			return []rainbow.Options{}, err
 		}
 
@@ -323,6 +431,8 @@ func GetAggregatedOrderBook(instruments []getOptionsOtokensOToken, provider stri
 
 		orderBook = append(orderBook, o)
 	}
+
+	sr.logStats()
 
 	return orderBook, nil
 }
