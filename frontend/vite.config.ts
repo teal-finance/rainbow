bec82ba3b7c1ab09c68f67996613a395d6dec8b3
Oliver <0uep@hessling.fr>
2021-11-03 20:58:45 +0100

    Add a hash in filenames to enable browser cache

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,14 +6,19 @@ import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ 
 export default defineConfig({
+  // https://rollupjs.org/guide/en/#outputentryfilenames
+  // https://rollupjs.org/guide/en/#outputchunkfilenames
+  // https://rollupjs.org/guide/en/#outputassetfilenames
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/js/[name].js`,
-        chunkFileNames: `assets/js/[name].js`,
-        assetFileNames: `assets/css/[name].[ext]`,
+        // The [hash] permits the use of "Cache-Control: max-age=604800"
+        // because [hash] will be different each time its content changes.
+        entryFileNames: `assets/js/[name]-[hash].js`,
+        chunkFileNames: `assets/js/[name]-[hash].js`,
+        assetFileNames: `assets/css/[name]-[hash].[ext]`,
       },
     },
   },
