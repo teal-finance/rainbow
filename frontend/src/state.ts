8b5d77ff95e1f154c1dba17e9ad17d24390be8a2
synw <synwx@protonmail.com>
2021-11-09 22:45:40 +0100

    Fix build, add mobile view and remove landing page

--- frontend/src/state.ts
+++ frontend/src/state.ts
@@ -3,9 +3,15 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
-
+import { useBreakpoints, breakpointsTailwind } from '@vueuse/core'
 import User from "./models/user";
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+const isMobile = breakpoints.isSmaller('sm');
+
+
 
 const user = new User();
 
-export { user }
+export { user, isMobile }
