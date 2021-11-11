a9ad8efce3d735c337a9d9c13d4f465ede93ac16
cryptohazard <jyezie@yahoo.fr>
2021-11-11 23:08:53 +0100

    fix psyoptions date

--- pkg/provider/psyoptions/psyoptions.go
+++ pkg/provider/psyoptions/psyoptions.go
@@ -23,38 +23,7 @@ const (
 	PsyQuoteCurrency = "USDC"
 	mainnet          = "https://solana-api.projectserum.com" // https://api.mainnet-beta.solana.com"
 	devnet           = "https://api.devnet.solana.com"
-	Expiration       = "2021-10-29 23:59:59"
 )
-
-func oldInstruments(coin string) []string {
-	switch coin {
-	case "ETH":
-		return []string{
-			"8A493gU55NfS4fCjDoLAiN57zPzWf6QQw31QQf1fd6iX",
-			"5pHcU2Gz8eCMwynLvz1AHSFoFbKkeTc7ufeqeG4spb99",
-			"8qgAVVE6eeJ9u32LvJupgW6NyNWPPFFK4xnXfGtDNeP4",
-			"FfkVR6ha6N9fcefGxfDFP97xL54Pc5ipiwnt1uuTpuyX",
-			"TLAzx53rb6pEDT3oWitTzg6YNe7BTERLDzujMdU8RQx",
-			"9A1m1JXgnMrdq3JkqEYx9rK3CcYm9EHDLeA6sn1hH3PP",
-			"HYmPvo8szh62QVaAfUAXR1eppvCfouUPpH68yE87UYmy",
-			"8fFcWuVaZSKoge4DCpMcNrR5nNXF2pbXCfBUxkMomgr5",
-		}
-
-	case "BTC":
-		return []string{
-			"8fhiAYm41RwtiX8WusCSpY617GWPt2LwUnCQcEeer78o",
-			"6at26sVk8vTYtLh4YDKvje4PDdgFJsNHHyoGw87WNszP",
-			"2gKrDsubuvYKxTkWdT5b44Qdd9QoBRTQQebUoQNnsesw",
-			"7W2LGEDpitCoXLC5xhzjUKiE4NnNkgoAstM2EyFt7MaS",
-			"9ugAWZCSgUKjL11fJE9Zjn4QVTdTkAkSLgPu9ZC8mcfD",
-			"ACdjLA5wPk31eUEqra9BFQ3MTXbHqZfdM1TRQPX8Hi28",
-			"2q5f1H8xT3tsBzQhwZC3BKnbKMb44fTuDGamZ6xUdZz2",
-			"DvohGwDZR9Z2siWBj2Xhgxd1qRScVCpywL3EoRbpon3p",
-		}
-	default:
-		return []string{}
-	}
-}
 
 func Options() ([]rainbow.Option, error) {
 	// instruments := append(oldInstruments("ETH"), oldInstruments("BTC")...)
@@ -87,7 +56,7 @@ func Options() ([]rainbow.Option, error) {
 			Name:          i.name(),
 			Type:          i.optionType(),
 			Asset:         i.asset(),
-			Expiry:        Expiration,
+			Expiry:        i.expiration(),
 			Strike:        i.strike(),
 			ExchangeType:  "DEX",
 			Chain:         "Solana",
@@ -145,4 +114,34 @@ func normalizeOrders(ctx context.Context, market *serum.MarketMeta, cli *rpc.Cli
 	}
 
 	return offers, totalSize, nil
+}
+
+func oldInstruments(coin string) []string {
+	switch coin {
+	case "ETH":
+		return []string{
+			"8A493gU55NfS4fCjDoLAiN57zPzWf6QQw31QQf1fd6iX",
+			"5pHcU2Gz8eCMwynLvz1AHSFoFbKkeTc7ufeqeG4spb99",
+			"8qgAVVE6eeJ9u32LvJupgW6NyNWPPFFK4xnXfGtDNeP4",
+			"FfkVR6ha6N9fcefGxfDFP97xL54Pc5ipiwnt1uuTpuyX",
+			"TLAzx53rb6pEDT3oWitTzg6YNe7BTERLDzujMdU8RQx",
+			"9A1m1JXgnMrdq3JkqEYx9rK3CcYm9EHDLeA6sn1hH3PP",
+			"HYmPvo8szh62QVaAfUAXR1eppvCfouUPpH68yE87UYmy",
+			"8fFcWuVaZSKoge4DCpMcNrR5nNXF2pbXCfBUxkMomgr5",
+		}
+
+	case "BTC":
+		return []string{
+			"8fhiAYm41RwtiX8WusCSpY617GWPt2LwUnCQcEeer78o",
+			"6at26sVk8vTYtLh4YDKvje4PDdgFJsNHHyoGw87WNszP",
+			"2gKrDsubuvYKxTkWdT5b44Qdd9QoBRTQQebUoQNnsesw",
+			"7W2LGEDpitCoXLC5xhzjUKiE4NnNkgoAstM2EyFt7MaS",
+			"9ugAWZCSgUKjL11fJE9Zjn4QVTdTkAkSLgPu9ZC8mcfD",
+			"ACdjLA5wPk31eUEqra9BFQ3MTXbHqZfdM1TRQPX8Hi28",
+			"2q5f1H8xT3tsBzQhwZC3BKnbKMb44fTuDGamZ6xUdZz2",
+			"DvohGwDZR9Z2siWBj2Xhgxd1qRScVCpywL3EoRbpon3p",
+		}
+	default:
+		return []string{}
+	}
 }
