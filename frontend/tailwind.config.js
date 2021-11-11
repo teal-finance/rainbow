e1422a882cc2d1024848767c08eeeb022841d971
synw <synwx@protonmail.com>
2021-11-11 18:31:56 +0100

    Add rainbow light and dark color palettes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,31 @@
 const colors = require('tailwindcss/colors');
 
+const rainbowColors = {
+  '50': '#f6fdff',
+  '100': '#ecfcff',
+  '200': '#d0f7fe',
+  '300': '#b4f1fe',
+  '400': '#7ce7fd',
+  '500': '#44ddfc',
+  '600': '#3dc7e3',
+  '700': '#33a6bd',
+  '800': '#298597',
+  '900': '#216c7b'
+};
+
+const rainbowDarkColors = {
+  '50': '#f4f8f8',
+  '100': '#e9f0f2',
+  '200': '#c8dade',
+  '300': '#a6c4ca',
+  '400': '#6498a3',
+  '500': '#216c7b',
+  '600': '#1e616f',
+  '700': '#19515c',
+  '800': '#14414a',
+  '900': '#10353c'
+}
+
 module.exports = {
   purge: {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
@@ -19,11 +45,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'rainbow': rainbowColors,
         'primary': {
-          DEFAULT: "#44ddfc",
+          DEFAULT: rainbowColors['500'],
           dark: colors.gray[700],
         },
         'primary-r': {
+          DEFAULT: "#FFFFFF",
+          dark: "#FFFFFF",
+        },
+        'secondary': {
+          DEFAULT: rainbowColors['700'],
+          dark: rainbowDarkColors['700'],
+        },
+        'secondary-r': {
           DEFAULT: "#FFFFFF",
           dark: "#FFFFFF",
         },
