538b0455110e3f474e39125354aca79f62b027a6
synw <synwx@protonmail.com>
2021-11-11 18:42:00 +0100

    Improve the rainbow color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,30 +1,47 @@
 const colors = require('tailwindcss/colors');
 
 const rainbowColors = {
-  '50': '#f6fdff',
-  '100': '#ecfcff',
-  '200': '#d0f7fe',
-  '300': '#b4f1fe',
-  '400': '#7ce7fd',
-  '500': '#44ddfc',
-  '600': '#3dc7e3',
-  '700': '#33a6bd',
-  '800': '#298597',
-  '900': '#216c7b'
+  '50': {
+    DEFAULT: '#f6fdff',
+    dark: '#f4f8f8',
+  },
+  '100': {
+    DEFAULT: '#ecfcff',
+    dark: '#e9f0f2',
+  },
+  '200': {
+    DEFAULT: '#d0f7fe',
+    dark: '#c8dade',
+  },
+  '300': {
+    DEFAULT: '#b4f1fe',
+    dark: '#a6c4ca',
+  },
+  '400': {
+    DEFAULT: '#7ce7fd',
+    dark: '#6498a3',
+  },
+  '500': {
+    DEFAULT: '#44ddfc',
+    dark: '#216c7b',
+  },
+  '600': {
+    DEFAULT: '#3dc7e3',
+    dark: '#1e616f',
+  },
+  '700': {
+    DEFAULT: '#33a6bd',
+    dark: '#19515c',
+  },
+  '800': {
+    DEFAULT: '#298597',
+    dark: '#14414a',
+  },
+  '900': {
+    DEFAULT: '#216c7b',
+    dark: '#10353c',
+  }
 };
-
-const rainbowDarkColors = {
-  '50': '#f4f8f8',
-  '100': '#e9f0f2',
-  '200': '#c8dade',
-  '300': '#a6c4ca',
-  '400': '#6498a3',
-  '500': '#216c7b',
-  '600': '#1e616f',
-  '700': '#19515c',
-  '800': '#14414a',
-  '900': '#10353c'
-}
 
 module.exports = {
   purge: {
@@ -47,7 +64,7 @@ module.exports = {
       colors: {
         'rainbow': rainbowColors,
         'primary': {
-          DEFAULT: rainbowColors['500'],
+          DEFAULT: rainbowColors['500'].DEFAULT,
           dark: colors.gray[700],
         },
         'primary-r': {
@@ -55,8 +72,8 @@ module.exports = {
           dark: "#FFFFFF",
         },
         'secondary': {
-          DEFAULT: rainbowColors['700'],
-          dark: rainbowDarkColors['700'],
+          DEFAULT: rainbowColors['700'].DEFAULT,
+          dark: rainbowColors['700'].dark,
         },
         'secondary-r': {
           DEFAULT: "#FFFFFF",
