c863a70e253a2cf8aa82671b4534f4b727981d05
Oliver <0uep@hessling.fr>
2021-11-14 22:39:53 +0100

    Revert "Update strike change (number > string) in frontend"
    This reverts commit 861de1b263f4cbeb5db60b0ac95b9a86d5e777af.

--- frontend/src/models/options/types.ts
+++ frontend/src/models/options/types.ts
@@ -17,7 +17,7 @@ interface CallPutContract {
   asset: string;
   expiry: string;
   call: LimitContract;
-  strike: string;
+  strike: number;
   put: LimitContract;
 }
 
@@ -47,7 +47,7 @@ type RowType = {
   asset: string;
   expiry: Date;
   call: CallType;
-  strike: string;
+  strike: number;
   put: PutType;
 }
 
