b1ce01a318d40bef3ad61dfbe78029d0d1279063
arad1990 <arad1990@protonmail.com>
2021-11-14 20:15:10 +0330

    change id to class

--- frontend/src/components/OptionsDatatable.vue
+++ frontend/src/components/OptionsDatatable.vue
@@ -70,15 +70,9 @@ export default defineComponent({
         @apply bg-primary bg-opacity-10 dark:text-primary-r
       &.callAskSize, &.putAskSize, &.callAskPrice, &.putAskPrice
         @apply bg-primaryTable dark:bg-primaryTable-dark bg-opacity-10 dark:text-primary-r dark:bg-opacity-60
-<<<<<<< HEAD
-      &#callBidPrice, &#putBidPrice
-        @apply text-success dark:text-success-dark font-bold text-right
-      &#callAskPrice, &#putAskPrice
-=======
       &.callBidPrice, &.putBidPrice
         @apply text-success dark:text-success-dark font-bold text-left
       &.callAskPrice, &.putAskPrice
->>>>>>> change id to class
         @apply text-danger dark:text-danger-dark font-bold text-left
     tr
       @apply border-gray-200 border-b dark:border-gray-700
