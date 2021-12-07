3de35f2acd175c208d91e5fefdb41a06886042df
kahlys <youennlaborde@live.fr>
2021-12-07 19:18:22 +0100

    backend new storage sqlite

---
+++ pkg/rainbow/storage/sqlite/db.go
@@ -0,0 +1,108 @@
+package sqlite
+
+import (
+	"github.com/teal-finance/rainbow/pkg/rainbow"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Option struct {
+	gorm.Model
+	Name          string
+	Type          string
+	Asset         string
+	Expiry        string
+	ExchangeType  string
+	Chain         string
+	Layer         string
+	Provider      string
+	QuoteCurrency string
+	Strike        float64
+	// Bid           []Order `json:"bid"`
+	// Ask           []Order `json:"ask"`
+}
+
+func (o Option) toOptions() rainbow.Option {
+	return rainbow.Option{
+		Name:          o.Name,
+		Type:          o.Type,
+		Asset:         o.Asset,
+		Expiry:        o.Expiry,
+		ExchangeType:  o.ExchangeType,
+		Chain:         o.Chain,
+		Layer:         o.Layer,
+		Provider:      o.Provider,
+		QuoteCurrency: o.QuoteCurrency,
+		Strike:        o.Strike,
+	}
+}
+
+type DB struct {
+	*gorm.DB
+}
+
+func NewDB(file string) (*DB, error) {
+	db, err := gorm.Open(sqlite.Open(file), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&Option{})
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{db}, nil
+}
+
+func (db *DB) InsertOptions(provider string, options []rainbow.Option) error {
+	for _, o := range options {
+		o.Provider = provider
+		res := db.Create(&Option{
+			Name:          o.Name,
+			Type:          o.Type,
+			Asset:         o.Asset,
+			Expiry:        o.Expiry,
+			ExchangeType:  o.ExchangeType,
+			Chain:         o.Chain,
+			Layer:         o.Layer,
+			Provider:      o.Provider,
+			QuoteCurrency: o.QuoteCurrency,
+			Strike:        o.Strike,
+		})
+		if res.Error != nil {
+			return res.Error
+		}
+	}
+	return nil
+}
+
+func (db *DB) GetOptions(provider string) ([]rainbow.Option, error) {
+	options := []Option{}
+	res := db.Where("provider = ?", provider).Find(&options)
+	if res.Error != nil {
+		return []rainbow.Option{}, res.Error
+	}
+
+	results := make([]rainbow.Option, len(options))
+	for i := range options {
+		results[i] = options[i].toOptions()
+	}
+
+	return results, res.Error
+}
+
+func (db *DB) GetAllOptions() ([]rainbow.Option, error) {
+	options := []Option{}
+	res := db.Find(&options)
+	if res.Error != nil {
+		return []rainbow.Option{}, res.Error
+	}
+
+	results := make([]rainbow.Option, len(options))
+	for i := range options {
+		results[i] = options[i].toOptions()
+	}
+
+	return results, res.Error
+}
