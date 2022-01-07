3efacefbb7596edbf630a161d36184980f4615da
Oliver <0uep@hessling.fr>
2022-01-07 23:04:51 +0100

    frontend: reuse api.url in graphql.ts

--- frontend/src/api/graphql.ts
+++ frontend/src/api/graphql.ts
@@ -1,16 +1,15 @@
 import { GraphQLClient, gql } from 'graphql-request'
+import api from '.';
 import { allQuery } from './queries';
 
-const addr = import.meta.env.VITE_ADDR as string || "http://localhost:8090";
-
-const graphQLClient = new GraphQLClient(addr, {
+const graphQLClient = new GraphQLClient(api.url + "graphql", {
   //credentials: 'include',
   mode: 'cors',
 });
 
 async function query() {
-  const q = await graphQLClient.request(addr + "/graphql", gql`query ${allQuery}`);
-  console.log("Q", q)
+  const data = await graphQLClient.request(gql`query ${allQuery}`);
+  console.log("Q", JSON.stringify(data, undefined, 2))
 }
 
 export { query }
