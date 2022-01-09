b91016f286f58f1d2d8aa5254a4d1570e554ca33
cryptohazard <jyezie@yahoo.fr>
2022-01-09 17:09:20 +0100

    oubli 2 :hankey:

--- cmd/server/webserver.go
+++ cmd/server/webserver.go
@@ -14,13 +14,8 @@ import (
 	"github.com/teal-finance/garcon/webserver"
 )
 
-<<<<<<< HEAD:pkg/webserver/handler.go
-// Handler serves the static web files of the front-end.
-func Handler(g *garcon.Garcon, wwwDir string) http.Handler {
-=======
 // WebHandler serves the static web files of the front-end.
 func WebHandler(g *garcon.Garcon, wwwDir string) http.Handler {
->>>>>>> main:cmd/server/webserver.go
 	r := chi.NewRouter()
 
 	// Static website files
