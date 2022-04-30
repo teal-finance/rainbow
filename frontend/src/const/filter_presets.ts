6e5e3d06fe8c48a7b485872f4a5c05c4873def15
cryptohazard <jyezie@yahoo.fr>
2022-05-01 00:33:10 +0200

    more presets

--- frontend/src/const/filter_presets.ts
+++ frontend/src/const/filter_presets.ts
@@ -1,5 +1,11 @@
 const assetsPresets = {
-  'BTC and ETH only': {
+  'BTC and ETH and SOL': {
+    'defaultValue': false,
+    'BTC': true,
+    'ETH': true,
+    'SOL': true,
+  },
+  'BTC and ETH': {
     'defaultValue': false,
     'BTC': true,
     'ETH': true,
@@ -11,13 +17,29 @@ const filterPresets: Record<string, any> = {
     assets: { 'defaultValue': true },
     providers: { 'defaultValue': true },
   },
-  'BTC and ETH only all providers': {
-    assets: assetsPresets['BTC and ETH only'],
+  'BTC and ETH and SOL on all providers': {
+    assets: assetsPresets['BTC and ETH and SOL'],
     providers: { 'defaultValue': true }
   },
-  'BTC and ETH on Opyn': {
-    assets: assetsPresets['BTC and ETH only'],
-    providers: { 'defaultValue': false, 'Opyn': true }
+  'BTC and ETH on Deribit': {
+    assets: assetsPresets['BTC and ETH'],
+    providers: { 'defaultValue': false, 'Deribit': true }
+  },
+  'CEX': {
+    assets: { 'defaultValue': true },
+    providers: { 'defaultValue': false, 'Deribit': true }
+  },
+  'DEX': {
+    assets: { 'defaultValue': true },
+    providers: { 'defaultValue': false, 'Lyra': true, 'Opyn': true,'Zeta':true,'PsyOptions':true }
+  },
+  'BTC': {
+    assets: { 'defaultValue': false, 'BTC': true },
+    providers: { 'defaultValue': true },
+  },
+  'ETH': {
+    assets: { 'defaultValue': false, 'ETH': true },
+    providers: { 'defaultValue': true },
   },
   'SOL': {
     assets: { 'defaultValue': false, 'SOL': true },
