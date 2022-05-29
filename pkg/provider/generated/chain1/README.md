d62b9322f49ab39ef0e4f25bcff9ed55e2d9ad47
Oliver <0uep@hessling.fr>
2022-05-29 23:17:53 +0200

    ✅ Add a complete GraphQL Go-code generation

---
+++ pkg/provider/generated/chain1/README.md
@@ -0,0 +1,4 @@
+# Working example
+
+Adapted from:
+<https://github.com/graphprotocol/graph-client/blob/main/examples/cross-chain/.graphclient/sources/Chain1/schema.graphql>
--- pkg/tools/generate.go
+++ pkg/tools/generate.go
@@ -3,5 +3,6 @@ package tools
 // Generate Go code to send GraphQL query.
 //go:generate go run github.com/Khan/genqlient ../provider/generated/opyn/config.yaml
 //go:generate go run github.com/Khan/genqlient ../provider/generated/thales/config.yaml
+//go:generate go run github.com/Khan/genqlient ../provider/generated/chain1/genqlient.yaml
 
 // There is nothing here intentionally (see README.md).
