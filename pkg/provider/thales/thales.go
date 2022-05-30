b78b1959ad8e178abd46ac8b4668d1b9513e90c3
Oliver <0uep@hessling.fr>
2022-05-30 14:13:13 +0200

    ✨ Implement thales.Query()

--- pkg/provider/thales/thales.go
+++ pkg/provider/thales/thales.go
@@ -1,7 +1,36 @@
 package thales
 
+import (
+	"context"
+	"log"
+
+	"github.com/Khan/genqlient/graphql"
+
+	"github.com/teal-finance/rainbow/pkg/provider/the-graph/thales"
+)
+
 type Provider struct{}
 
 func (Provider) Name() string {
 	return "Thales"
+}
+
+func Query() []thales.MarketsMarketsMarket {
+	const url = "https://api.thegraph.com/subgraphs/name/thales-markets/thales-optimism"
+	const skip = 0
+	const first = 100
+	const minExpiry = 1651300000
+
+	graphqlClient := graphql.NewClient(url, nil)
+
+	resp, err := thales.Markets(context.TODO(), graphqlClient, skip, first, minExpiry)
+	if err != nil {
+		log.Print("ERR: ", err)
+	}
+	if resp == nil {
+		log.Print("ERR: resp=nil")
+		return nil
+	}
+
+	return resp.Markets
 }
