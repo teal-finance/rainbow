32efb8e9fdc46de319cc24bf07f81bb7d68b783a
Oliver <0uep@hessling.fr>
2022-07-03 01:48:08 +0200

    ✅ Update PoC for Thales

--- poc/thales.golang/main.go
+++ poc/thales.golang/main.go
@@ -19,8 +19,24 @@ func main() {
 	markets := thales.QueryAllMarkets()
 	log.Print(markets)
 
-	m := thales.QueryMarket("0x5416c2ab11c7852ed9648aa006ee69d412c735c9")
+	m := thales.QueryMarket("0xd9e137e43fb366fb500553c2066b805616d9b0d4")
 	log.Print(m)
+
+	httpResponse := `{"data":{"markets":[{
+		"id":"0xd9e137e43fb366fb500553c2066b805616d9b0d4",
+		"timestamp":"1656423710",
+		"creator":"0x5027ce356c375a934b4d1de9240ba789072a5af1",
+		"currencyKey":"0x4f484d0000000000000000000000000000000000000000000000000000000000",
+		"strikePrice":"18000000000000000000",
+		"maturityDate":"1657526400",
+		"expiryDate":"1673251200",
+		"isOpen":true,
+		"poolSize":"10000000000000000",
+		"longAddress":"0x252b0c0d3268ca8c54eb0b4ddfdb252bb2681f0d",
+		"shortAddress":"0xa3487b9c100c25a162d2df56dc00b4fdf4cb4d15",
+		"result":null,
+		...`
+	log.Print("HTTP response from TheGraph:", httpResponse)
 }
 
 func name(c []byte) string {
