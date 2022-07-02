d82c38c1ed5e8097107d8a9af82966a483b0e07b
cryptohazard <jyezie@yahoo.fr>
2022-07-03 00:00:11 +0200

    add new query

--- poc/thegraph/main.go
+++ poc/thegraph/main.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/Khan/genqlient/graphql"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/spewerspew/spew"
 
 	"github.com/teal-finance/rainbow/pkg/provider/the-graph/thales"
 )
@@ -24,7 +23,7 @@ func main() {
 	const url = "https://api.thegraph.com/subgraphs/name/thales-markets/thales-optimism"
 	//const url = "https://api.thegraph.com/subgraphs/name/thales-markets/thales-polygon"
 	graphqlClient := graphql.NewClient(url, nil)
-	resp, err := thales.Market(context.TODO(), graphqlClient, "0x5416c2ab11c7852ed9648aa006ee69d412c735c9")
+	resp, err := thales.AllLive(context.TODO(), graphqlClient) //, "0x5416c2ab11c7852ed9648aa006ee69d412c735c9")
 	if err != nil {
 		log.Print("ERR: ", err)
 	}
@@ -33,7 +32,7 @@ func main() {
 		log.Print("ERR: resp=nil")
 		return
 	}
-	spew.Dump(resp.Market)
+	//spew.Dump(len(resp.Markets))
 	/*
 		var t int64 = 1654041600 //01-JUN-2022
 		response, err := thales.Markets(context.TODO(), graphqlClient, 0, 0, t)
