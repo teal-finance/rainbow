ce965568b4f2446b50c5ade21ddef7d7117ca208
Oliver <0uep@hessling.fr>
2022-07-03 22:46:26 +0200

    🎨 Update the Thales PoC

--- poc/thales.golang/main.go
+++ poc/thales.golang/main.go
@@ -7,48 +7,28 @@
 package main
 
 import (
-	"bytes"
 	"log"
 
-	"github.com/ethereum/go-ethereum/common"
-
+	"github.com/spewerspew/spew"
 	"github.com/teal-finance/rainbow/pkg/provider/thales"
 )
 
 func main() {
 	log.Print("-------- thales.QueryAllMarkets --------")
 	all := thales.QueryAllMarkets()
-	log.Print(all)
+	log.Print("len = ", len(all))
+	if len(all) > 0 {
+		spew.Dump(all[0])
+	}
 
 	log.Print("-------- thales.QueryMarkets --------")
 	markets := thales.QueryMarkets()
-	log.Print(markets)
+	log.Print("len = ", len(markets))
+	if len(markets) > 0 {
+		spew.Dump(markets[0])
+	}
 
 	log.Print("-------- thales.QueryMarket --------")
 	m := thales.QueryMarket("0x5416c2ab11c7852ed9648aa006ee69d412c735c9")
-	log.Print(m)
-
-	httpResponse := `{"data":{"markets":[{
-		"id":"0xd9e137e43fb366fb500553c2066b805616d9b0d4",
-		"timestamp":"1656423710",
-		"creator":"0x5027ce356c375a934b4d1de9240ba789072a5af1",
-		"currencyKey":"0x4f484d0000000000000000000000000000000000000000000000000000000000",
-		"strikePrice":"18000000000000000000",
-		"maturityDate":"1657526400",
-		"expiryDate":"1673251200",
-		"isOpen":true,
-		"poolSize":"10000000000000000",
-		"longAddress":"0x252b0c0d3268ca8c54eb0b4ddfdb252bb2681f0d",
-		"shortAddress":"0xa3487b9c100c25a162d2df56dc00b4fdf4cb4d15",
-		"result":null,
-		...`
-	log.Print("-------- HTTP response from TheGraph --------")
-	log.Print(httpResponse)
-}
-
-func name(c []byte) string {
-	l := common.HexToHash(string(c))
-	b := l.Bytes()
-	b = bytes.Trim(b, "\x00")
-	return string(b)
+	spew.Dump(m)
 }
