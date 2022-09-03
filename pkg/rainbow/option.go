f88dbc95a87e9af79b830402d336ac599329c733
cryptohazard <jyezie@yahoo.fr>
2022-09-03 02:34:09 +0200

    prepare IV

--- pkg/rainbow/option.go
+++ pkg/rainbow/option.go
@@ -9,19 +9,24 @@ import "fmt"
 
 type Option struct {
 	Name          string  `json:"name"`      // ASSET-DATE-Strike-OptionsType
-	Type          string  `json:"type"`      // CALL / PUT
-	Asset         string  `json:"asset"`     // ETH, BTC, SOL
+	Type          string  `json:"type"`      // CALL / PUT  // TODO add exotic like binary and perp(squeeth)
+	Asset         string  `json:"asset"`     // ETH, BTC, SOL, ...
 	Expiry        string  `json:"expiry"`    // Expiry date in format 2021-12-31
 	ExchangeType  string  `json:"exchange"`  // CEX / DEX
 	Chain         string  `json:"chain"`     // Ethereum, Solana and "–" for CEX (Deribit)
 	Layer         string  `json:"layer"`     // L1, L2 and "–" for CEX (Deribit)
 	LayerName     string  `json:"layername"` // name of the layer 1/2. "-" for CEX
-	Provider      string  `json:"provider"`  // Opyn, Lyra, Thales, Deribit, Psyoptions
-	QuoteCurrency string  `json:"currency"`  // ETH, BTC, USDT...
+	Provider      string  `json:"provider"`  // Opyn, Lyra, Thales, Deribit, Psyoptions, Zeta, Delta
+	QuoteCurrency string  `json:"currency"`  // USD // TODO add the real underlying sUSD, USDC, USDT...
 	URL           string  `json:"url"`       // link to market @ provider.
-	Bid           []Order `json:"bid"`
-	Ask           []Order `json:"ask"`
-	Strike        float64 `json:"strike"`
+	Bid           []Order `json:"bid"`       // Bid = buying offers
+	BidIV         float64 `json:"bidIV"`     // Implied Volatility computed from the bid
+	Ask           []Order `json:"ask"`       // Ask = selling offers
+	AskIV         float64 `json:"askIV"`     // Implied Volatility computed from the ask
+	MarketIV      float64 `json:"markIV"`    // When it is present on the provider, we store their Market IV
+	// see https://corporatefinanceinstitute.com/resources/knowledge/trading-investing/option-greeks/
+	Greeks TheGreeks `json:"greeks"` // Greeks measure the sensitivity of an option’s price to its the underlying determining parameters.
+	Strike float64   `json:"strike"` //
 }
 
 // TODO put standard name here.
@@ -29,10 +34,18 @@ func (o *Option) OptionName() string {
 	if o.Name != "" {
 		return o.Name
 	}
-	return o.Provider + "-" + o.LayerName + "-" + o.Asset + "-" + fmt.Sprintf("%.3f", o.Strike) + "-" + o.Type + "-" + o.Expiry
+	return o.Provider + "-" + o.LayerName + "-" + o.Asset + "-" + fmt.Sprintf("%.2f", o.Strike) + "-" + o.Type + "-" + o.Expiry
 }
 
 type Order struct {
 	Price float64 `json:"px"`
 	Size  float64 `json:"size"`
+}
+
+type TheGreeks struct {
+	Vega  float64 `json:"vega"`
+	Theta float64 `json:"theta"`
+	Rho   float64 `json:"rho"`
+	Gamma float64 `json:"gamma"`
+	Delta float64 `json:"delta"`
 }
