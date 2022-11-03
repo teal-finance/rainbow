89a8f943e8aa30c5a1a99c54622d10193d34c8c3
cryptohazard <jyezie@yahoo.fr>
2022-11-03 12:09:42 +0100

    change default filter

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -7,5 +7,5 @@ import { User as SwUser } from "@snowind/state";
 import { useStorage } from "@vueuse/core";
 
 export default class User extends SwUser {
-  currentPreset = useStorage("preset", "All");
+  currentPreset = useStorage("preset", "DEXes");
 }
