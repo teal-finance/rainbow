64f510315b589b59eefea1bd0e836656ec9d3c61
cryptohazard <jyezie@yahoo.fr>
2023-04-28 00:11:15 +0200

    pause synquote from default

--- pkg/provider/providers.go
+++ pkg/provider/providers.go
@@ -14,7 +14,6 @@ import (
 	"github.com/teal-finance/rainbow/pkg/provider/deltaexchange"
 	"github.com/teal-finance/rainbow/pkg/provider/deribit"
 	"github.com/teal-finance/rainbow/pkg/provider/lyra"
-	"github.com/teal-finance/rainbow/pkg/provider/synquote"
 	"github.com/teal-finance/rainbow/pkg/provider/thales"
 	"github.com/teal-finance/rainbow/pkg/rainbow"
 )
@@ -26,8 +25,8 @@ func AllProviders() []rainbow.Provider {
 	return []rainbow.Provider{
 		&deribit.Provider{},
 		lyra.Provider{},
-		synquote.Provider{},
-		// zetamarkets.Provider{}, // paused platform due to current market conditions
+		//synquote.Provider{},		// paused cause they changed layer and I didn't get the new API link
+		// zetamarkets.Provider{},	// paused platform due to current market conditions
 		thales.Provider{},        // Thales = exotic options -> https://teal.finance/rainbow/exotic
 		deltaexchange.Provider{}, // last because slow (rate limit)
 	}
