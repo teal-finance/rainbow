fd92b753f53a8f843fd998927c3f9a1e468997f4
cryptohazard <jyezie@yahoo.fr>
2023-11-04 02:12:12 +0100

    init new work branches

---
+++ pkg/provider/aevo/aevo.go
@@ -0,0 +1,11 @@
+// Copyright 2023 Teal.Finance/Rainbow contributors
+// This file is part of Teal.Finance/Rainbow,
+// a screener for DeFi options under the MIT License.
+// SPDX-License-Identifier: MIT
+
+package aevo
+
+// https://api-docs.aevo.xyz/reference/getmarkets
+//	url := "https://api.aevo.xyz/markets?instrument_type=OPTION"
+
+// https://api-docs.aevo.xyz/reference/getorderbook
---
+++ pkg/provider/bybit/bybit.go
@@ -0,0 +1,6 @@
+// Copyright 2023 Teal.Finance/Rainbow contributors
+// This file is part of Teal.Finance/Rainbow,
+// a screener for DeFi options under the MIT License.
+// SPDX-License-Identifier: MIT
+
+package bybit
